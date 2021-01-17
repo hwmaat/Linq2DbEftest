cca7fbe0f4fd438d3f2dc9b5aefb4ebabed2a1ad
Herman Maat <hwmaat@outlook.com>
2021-01-17 23:23:38 +0100

    upd

--- Linq2DbEftest/Migrations/SqliteMigrations/20210117222229_update1.Designer.cs
+++ Linq2DbEftest/Migrations/SqliteMigrations/20210117222229_update1.Designer.cs
@@ -2,14 +2,16 @@
 using Linq2DbEftest.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Linq2DbEftest.Migrations.SqliteMigrations
 {
     [DbContext(typeof(SqlLiteContext))]
-    partial class SqlLiteContextModelSnapshot : ModelSnapshot
+    [Migration("20210117222229_update1")]
+    partial class update1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
